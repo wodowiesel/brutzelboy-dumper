9667839d038dcdd25c9e826d29dbc55d06ce07a5
WodoWiesel <wodowiesel@quantentunnel.de>
2025-05-28 20:59:24 +0200

    Update and rename py-reader-kombi.bat to Brutzelboy-Dumper-py-kombi.bat
    rename

---
+++ Brutzelboy-Dumper-py-kombi.bat
@@ -0,0 +1,4 @@
+:: cd .\brutzelboy-dumper\
+echo Starting Brutzelboy-Dumper Py Interface...
+call python Brutzelboy-Dumper_v1.9.0_Py_Interface_KOMBI.py
+PAUSE
--- py-reader-kombi.bat
+++
@@ -1,4 +0,0 @@
-:: cd C:\Users\maria\OneDrive\Desktop\rettung\kombi
-echo Starting Py Interface...
-call python GB-Dumper_v1.8_Rev1.3.6_Py_Interface_KOMBI.py
-PAUSE
